4a338f44087d22141c041cc6c35f607b8d8fb4eb
Cadu <carlos.eduardo.oliver.alves@gmail.com>
2026-03-11 09:44:31 -0300

    Add web scraping script to extract trending GitHub repositories

---
+++ web_scraping/trending_github.py
@@ -0,0 +1,56 @@
+import json
+import os
+from playwright.sync_api import sync_playwright
+
+BASE_URL = "https://github.com/trending?since=monthly"
+
+def run(playwright):
+
+    # Definindo o navegador
+    browser = playwright.chromium.launch(headless=False)
+
+    # Create a new browser context
+    context = browser.new_context(
+        user_agent="your-custom-user-agent",
+        viewport={"width": 800, "height": 600},
+        bypass_csp=True
+    )
+
+    page = context.new_page()
+    page.goto(BASE_URL)
+
+    # Extraindo os dados
+    repo_trendings = page.locator("article.Box-row")
+
+    data = []
+
+    for i in range(repo_trendings.count()):
+        repo = repo_trendings.nth(i)
+
+        name = repo.locator("h2 a")
+        language = repo.locator('span[itemprop="programmingLanguage"]')
+        stars = repo.locator('a[href$="/stargazers"]')
+        forks = repo.locator('a[href$="/forks"]')
+
+        repository = {
+            "name": name.inner_text().strip(),
+            "language": language.inner_text().strip() if language.count() > 0 else None,
+            "stars_total": stars.inner_text().strip() if stars.count() > 0 else None,
+            "forks": forks.inner_text().strip() if forks.count() > 0 else None,
+            "url": f"https://github.com{name.get_attribute('href')}"
+        }
+
+        data.append(repository)
+
+    print(data)
+
+    os.makedirs("data", exist_ok=True)
+
+    with open("data/trending_repositories.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+    # Fechando o navegador
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)            
